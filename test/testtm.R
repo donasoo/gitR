83f8ad601224e8c550d13504ca649a611dba6964
donasoo <habsburgh@gmail.com>
2016-04-23 22:49:39 +0800

    test tm test

--- test/testtm.R
+++ test/testtm.R
@@ -1,5 +1,6 @@
 vignette("tm") 
 library(tm)
+library(SnowballC)
 txt <- system.file("texts", "txt", package = "tm")
 txt
 docs <- c("This is a text.", "This another one.")
@@ -19,3 +20,18 @@ xas <- strsplit(assignments[, 1], split="[,]")
 xas
 states = rownames(USArrests)
 states
+
+
+data <- readLines("http://www.r-bloggers.com/wp-content/uploads/2016/01/vent.txt") # from: http://www.wvgazettemail.com/
+df <- data.frame(data)
+textdata <- df[df$data, ]
+textdata = gsub("[[:punct:]]", "", textdata)
+
+jay <- read.delim(file = '108479.txt', stringsAsFactors = F, header = F)
+unlist(strsplit(jay[,1], " "))
+unlist(strsplit("a.b.c", "."))
+unlist(strsplit("a.b.c", "\\."))
+
+jay <- jay[,1]
+unlist(strsplit(jay.pro, " |,|."))
+strsplit(jay.pro, " |,|.")
