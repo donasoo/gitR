9f1e08e07b0bd59d1ebbabb2242e7cb04358c72d
donasoo <habsburgh@gmail.com>
2019-04-30 14:11:00 +0800

    v0.1

--- G101/auditplot.R
+++ G101/auditplot.R
@@ -4,35 +4,37 @@ library(tidyverse)
 library(stringr)
 library(cowplot)
 
-plotsingle <- function(zoneid, plotcol){
+# 画单张图
+# 输入zoneid：地区行政代码
+# 输入plotvars: x,y,color,size 4个变量，默认为乡镇、行政区划面积，乡级类型，人口
+plotsingle <- function(zoneid='330802', plotvars=c('Z00', 'Z07', 'Z01', 'Z19')){
   
-  xlabel <- varnames[varnames$varid==plotcol[1], 'name']
-  ylabel <- varnames[varnames$varid==plotcol[2], 'name']
+  xlabel <- varnames[varnames$varid==plotvars[1], 'name']
+  ylabel <- varnames[varnames$varid==plotvars[2], 'name']
   clabel <- ''
   slabel <- ''
   
-  varid <- 'varid'
   zonelen <- str_count(zoneid)
   codelen <- str_count(allplotdata$code[1])
   
-  pdata <- allplotdata[str_sub(allplotdata$code, 1, zonelen)==zoneid, c('code', plotcol)]
+  pdata <- allplotdata[str_sub(allplotdata$code, 1, zonelen)==zoneid, c('code', plotvars)]
 
   if(nrow(pdata)<1) return()
   
-  if(length(plotcol) == 2){
-    pdata$ccol=1
-    pdata$scol=1
+  if(length(plotvars) == 2){
+    pdata$cvar=1
+    pdata$svar=1
   }else{
-    clabel <- varnames[varnames$varid==plotcol[3], 'name']
+    clabel <- str_c('颜色表示：\n',varnames[varnames$varid==plotvars[3], 'name'])
   }
   
-  if(length(plotcol) == 3){
-    pdata$scol=1
+  if(length(plotvars) == 3){
+    pdata$svar=1
   }else{
-    slabel <- varnames[varnames$varid==plotcol[4], 'name']
+    slabel <- str_c('大小表示：\n', varnames[varnames$varid==plotvars[4], 'name'])
   }
   
-  colnames(pdata) <- c('code', 'xcol', 'ycol', 'ccol', 'scol')
+  colnames(pdata) <- c('code', 'xvar', 'yvar', 'cvar', 'svar')
   pdata <- mutate(pdata, town=str_sub(as.character(code), zonelen+1, codelen))
   
   path <- str_c("./testplot/", zoneid)
@@ -40,47 +42,55 @@ plotsingle <- function(zoneid, plotcol){
     dir.create(path)
   }
   
-  cb_palette <- c('#B22234', "#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
+  cb_palette <- c('#B22234', "#096386", "#facf5a", "#00b7a8", "#692db7", "#F0E442",
                   "#0072B2", "#D55E00", "#CC79A7")
   
-  pp <- ggplot(pdata, aes(x=as.factor(xcol), y=ycol, color=ccol, size=scol))
+  pp <- ggplot(pdata, aes(x=xvar, y=yvar, color=cvar, size=svar))
   pp <- pp+ geom_point(alpha=0.8)+geom_text(aes(label=town), size=4, vjust = "inward")
   pp <- pp+labs(x=xlabel, 
                 y=ylabel, 
                 color=clabel,
                 size=slabel,
-                title= paste(varid))+
+                title= titleword)+
     theme(axis.text.x = element_blank())+
     scale_color_manual(values=cb_palette)
   
+  if(is.numeric(pdata$xvar)) pp <-pp+ geom_abline(intercept=0, slope=1)
   if(nchar(clabel)==0) pp <- pp+guides(color=F)
   if(nchar(slabel)==0) pp <- pp+guides(size=F)
-  pp
-  pb <- ggplot(pdata, aes(y=ycol))+geom_boxplot()+
-    geom_hline(yintercept = mean(pdata$ycol), color="orange",size=1)+
+  pb <- ggplot(pdata, aes(y=yvar))+geom_boxplot()+
+    geom_hline(yintercept = mean(pdata$yvar), color="orange",size=1)+
     labs(x='橙色为均值', y="")+
     theme(axis.text.y = element_blank(),axis.text.x = element_blank())
-  
-  
+
   p <- plot_grid(pp, pb, labels = c("", ""), align = "h", rel_widths = c(4, 1))
-  p
-  filename <- str_c('testplot/', zoneid, '/','最小前50位 ', varid,as.character('.png'))
-  ggsave(filename, plot=p, width = 8, height = 6)
+  filename <- str_c('testplot/', zoneid, '/',fnprefix, plotvars[1], xlabel, '-',plotvars[2],  ylabel, '.png')
+  ggsave(filename, plot=p, width = 9, height = 6)
   
 }
 
-plotzone <- function(zoneid){
+#plotsingle(zoneid, plotcol)
+
+# 批量画一个地区的
+# zoneid：地区
+# 输入plotvars: x,y,color,size 4个变量，默认为乡镇、行政区划面积，乡级类型，人口
+# yvars，批量的y变量，默认为全部
+plotzone <- function(zoneid='330802', plotvars=c('Z00', 'Z00', 'Z01', 'Z19'), yvars=colnames(allplotdata)){
   
-  for(varid in vars){
-    print(varid)
-    plotsingle(zoneid, varid)
+  for(yvar in yvars){
+    plotvars[2] <- yvar
+    plotsingle(zoneid, plotvars)
   }
 }
 
-Sys.time()
-
-citys <- as.character(3301:3311) 
-for(zoneid in citys){
-  print(zoneid)
-  plotzone(zoneid)
+# 批量多地区多图
+# grade：地区级别，行政区划码的位数，6为县
+# 输入plotvars: x,y,color,size 4个变量，默认为乡镇、行政区划面积，乡级类型，人口
+# yvars，批量的y变量，默认为全部
+plotall <- function(grade=6, plotvars=c('Z00', 'Z00', 'Z01', 'Z19'), yvars=colnames(allplotdata)){
+ 
+  for(zone in unique(str_sub(allplotdata$code, 1, grade))){
+    plotzone(zone, plotvars, yvars)
+  }
 }
+
--- G101/g101.R
+++ G101/g101.R
@@ -18,8 +18,30 @@ gzd <- mutate(gzd,
               Z00=str_sub(as.character(Z00), 1, 9),
               code=Z00)
 
-varnames <- gzvar
-allplotdata <- gzd
-plotcol <- c('Z00', 'Z07', 'Z01', 'Z19')
+##  test var
 zoneid <- '331082'
+
+# 设置全局变量
+# varnames  指标名称代码
+varnames <- gzvar
+# allplotdata需要画的数据集，横格式
+allplotdata <- select(gzd, -starts_with('X'))
+# plotcol： x y 颜色 大小 4个变量设置
+allmode <- c('Z00', 'Z12', 'Z01', 'Z19')
+# titleword： 标题
+titleword <- paste(zoneid, '乡镇卡审核用图')
+# fnprefix  文件名前缀
+fnprefix <- 'G101 '
+
+# 各类占村比
+z11_yvars <- c('Z12', 'Z13', 'Z14', 'Z15','Z16', 'Z17')
+z11_mode <- c('Z11', 'Z12', 'Z01', 'Z19')
+
+# 占耕地比
+z23_yvars <- c('Z24', 'Z25', 'Z26', 'Z27','Z28')
+z11_mode <- c('Z23', 'Z23', 'Z01', 'Z07')
+
+
+
 plotsingle(zoneid, plotcol)
+plotzone(zoneid, yvars)
